9463bd73e07367fe3219d57238a6dc9d1c9b523e
Tggtt <Tggtt@users.noreply.github.com>
2021-06-05 03:26:33 -0300

    Fixed background with repetition (tiling).

--- TheGame/Source/Main.hx
+++ TheGame/Source/Main.hx
@@ -141,7 +141,10 @@ class Main extends Sprite
 	private inline function placeTiles()
 	{ 
 		this.stage.color = 0x7e868f;
-		this.addChild(new Bitmap (Assets.getBitmapData ("assets/ajbm.png")));
+		this.graphics.beginFill();
+		this.graphics.beginBitmapFill(Assets.getBitmapData ("assets/ajbm.png"));
+		this.graphics.drawRect(0, 0, this.stage.stageWidth, this.stage.stageHeight);
+		this.graphics.endFill();
 
 		bookOpen = new Sprite();
 		bookOpen.addChild(new Bitmap (Assets.getBitmapData ("assets/ajmb2.png")));
